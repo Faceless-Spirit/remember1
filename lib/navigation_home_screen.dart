971ecbf8df8571a42b003ac217f373a4ede3abca
Syuq <tatd.wine@gmail.com>
2023-09-28 00:40:35 +0700

    🔧 fix(.metadata): update revision in version and migration sections 🔧 fix(ios/Runner.xcodeproj/project.pbxproj): update PRODUCT_BUNDLE_IDENTIFIER 🔧 fix(ios/Runner/Info.plist): update CFBundleDisplayName 🔧 fix(lib/main.dart): update import statement for app_theme.dart 🔧 fix(lib/navigation_home_screen.dart): update import statements for custom_drawer and screens 🔧 fix(macos/Runner.xcodeproj/project.pbxproj): update PRODUCT_BUNDLE_IDENTIFIER
    The changes in the `.metadata` file update the revision in the version and migration sections, ensuring that the correct revision is used.
    
    In the `ios/Runner.xcodeproj/project.pbxproj` file, the `PRODUCT_BUNDLE_IDENTIFIER` is updated to `com.example.remember` to match the new app name.
    
    The `ios/Runner/Info.plist` file is updated to change the `CFBundleDisplayName`
    
    🐛 fix(AppInfo.xcconfig): update PRODUCT_BUNDLE_IDENTIFIER to com.example.remember
    📦 chore(pubspec.yaml): update sdk version to '>=3.1.1 <4.0.0' and remove unused assets and dependencies
    
    The PRODUCT_BUNDLE_IDENTIFIER in AppInfo.xcconfig is updated to com.example.remember to match the desired bundle identifier for the application. In pubspec.yaml, the sdk version is updated to '>=3.1.1 <4.0.0' to ensure compatibility with the latest Flutter version. Additionally, unused assets and dependencies are removed from the file to declutter the project.

--- lib/navigation_home_screen.dart
+++ lib/navigation_home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:remember/app_theme.dart';
-import 'package:best_flutter_ui_templates/custom_drawer/drawer_user_controller.dart';
-import 'package:best_flutter_ui_templates/custom_drawer/home_drawer.dart';
-import 'package:best_flutter_ui_templates/feedback_screen.dart';
-import 'package:best_flutter_ui_templates/help_screen.dart';
-import 'package:best_flutter_ui_templates/home_screen.dart';
-import 'package:best_flutter_ui_templates/invite_friend_screen.dart';
+import 'package:remember/custom_drawer/drawer_user_controller.dart';
+import 'package:/custom_drawer/home_drawer.dart';
+import 'package:/feedback_screen.dart';
+import 'package:/invite_friend_screen.dart';
 import 'package:flutter/material.dart';
 
 class NavigationHomeScreen extends StatefulWidget {
