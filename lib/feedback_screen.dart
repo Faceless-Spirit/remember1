406ddff25229b47fa000d454d21653f91611c025
Syuq <tatd.wine@gmail.com>
2023-09-29 23:55:35 +0700

    🔀 chore(feedback_screen.dart): rearrange import statements The import statements in the feedback_screen.dart file have been rearranged to follow a consistent order. The import statement for the app_theme.dart file has been moved to the correct location.

--- lib/feedback_screen.dart
+++ lib/feedback_screen.dart
@@ -1,5 +1,5 @@
-import 'package:best_flutter_ui_templates/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:remember/theme/app_theme.dart';
 
 class FeedbackScreen extends StatefulWidget {
   @override
