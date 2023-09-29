7ea8e88ee97c73881523fb22f69a3a464f325dc9
Syuq <tatd.wine@gmail.com>
2023-09-30 01:41:38 +0700

    🚀 feat(login_content.dart): add navigation to GoogleBottomBar screen 🚀 feat(pubspec.yaml): add salomon_bottom_bar dependency The login button in the LoginContent component now navigates to the GoogleBottomBar screen when pressed. This allows users to access the GoogleBottomBar functionality. Additionally, the salomon_bottom_bar dependency has been added to the pubspec.yaml file to support the implementation of the GoogleBottomBar screen.

--- lib/screens/login_screen/components/login_content.dart
+++ lib/screens/login_screen/components/login_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:remember/screens/bottom_bar/google_bottom_bar.dart';
 import 'package:remember/utils/helper_functions.dart';
 
 import '../../../utils/constants.dart';
@@ -56,7 +57,13 @@ class _LoginContentState extends State<LoginContent>
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 135, vertical: 16),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const GoogleBottomBar(
+                            ),));
+        },
         style: ElevatedButton.styleFrom(
           padding: const EdgeInsets.symmetric(vertical: 14),
           shape: const StadiumBorder(),
